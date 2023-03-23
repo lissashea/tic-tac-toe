542e68ea7110cf201030db361c2bb69ab264e4e5
“Lissa <lwarshaw674@gmail.com>
2023-03-23 13:31:17 -0400

    update colors to x o

--- main.js
+++ main.js
@@ -16,19 +16,16 @@ document.body.append(table)
 let squares = document.querySelectorAll('td');
 let message = document.querySelector('#message');
 let reset = document.querySelector('#reset');
-let currentPlayer = 'Red';
+let currentPlayer = 'X';
 let clickedSquares = [];
-let red = "X"
-let blue = "O"
 
 
 squares.forEach(function(square) {
   square.addEventListener('click', () => {
-    if (!square.classList.contains('Red') && !square.classList.contains('Blue')) {
+    if (!square.classList.contains('X') && !square.classList.contains('O')) {
       square.classList.add(currentPlayer);
-      square.style.backgroundColor = currentPlayer;
-      square.textContent = currentPlayer === 'Red' ? 'X' : 'O'; 
-      currentPlayer = currentPlayer === 'Red' ? 'Blue' : 'Red';
+      square.textContent = currentPlayer === 'X' ? 'X' : 'O'; 
+      currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
       clickedSquares.push(square);
       message.textContent = `${currentPlayer}'s turn`;
     }
@@ -37,10 +34,10 @@ squares.forEach(function(square) {
 
 reset.addEventListener('click', () => {
   squares.forEach(function(square) {
-    square.classList.remove('Red','Blue');
+    square.classList.remove('X','O');
     square.style.backgroundColor = '';
     square.textContent = '';
   });
-  message.textContent = 'Red\'s turn';
-  currentPlayer = 'Red';
+  message.textContent = 'X\'s turn';
+  currentPlayer = 'X';
   });
