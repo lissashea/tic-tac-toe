2aaddcd58f84c5aed7b451ce6dea914fc4e6ecc4
“Lissa <lwarshaw674@gmail.com>
2023-03-23 12:25:31 -0400

    stlyes

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
+let table = document.createElement('table');
 
-let table = document.createElement("table");
-
-for (let i = 0; i < 3; i ++) {
- let row = document.createElement("row");
+for (let i = 0; i < 3; i++) {
+  let row = document.createElement('tr')
     for (let j = 0; j < 3; j++) {
-      let cell = document.createElement("tr")
-      cell.setAttribute("id", `square:${i * 3 + j + 1}`)
+      let cell = document.createElement('td');
+      cell.setAttribute('id',`square${i * 3 + j + 1}`);
       row.append(cell);
     }
-    table.append(row);
+      table.append(row);
 }
 
 document.body.append(table);
 
+let squares = document.querySelectorAll('td');
+let message = document.querySelector('#message');
+let reset = document.querySelector('#reset');
+let currentPlayer = 'red';
+let clickedSquares = [];
+
+squares.forEach(function(square) {
+  square.addEventListener('click', () => {
+    if (!square.classList.contains('red') && !square.classList.contains('blue')) {
+      square.classList.add(currentPlayer);
+      square.style.backgroundColor = currentPlayer;
+      currentPlayer = currentPlayer === 'red' ? 'blue' : 'red';
+      clickedSquares.push(square);
+      message.textContent = `${currentPlayer}'s turn`;
+    }
+  })
+});
+
+reset.addEventListener('click', () => {
+  squares.forEach(function(square) {
+    square.classList.remove('red','blue');
+  })
+  message.textContent = 'Red\'s turn';
+  currentPlayer = 'red';
+});
